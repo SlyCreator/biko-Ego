14fbba77c5f894c5d1e8034cf063621b4abd9dca
slycreator <chinonso1amaechi@gmial.com>
2021-04-08 10:43:28 +0100

    added db configuration

--- main.go
+++ main.go
@@ -12,24 +12,24 @@ import (
 
 func main()  {
 	 r := gin.Default()
-	 authRoute := r.Group("api/auth")
-	 {
-	 	authRoute.POST("/login",authController.Login)
-	 	authRoute.POST("/register",authController.Register)
-	 	authRoute.POST("/reset_password",authController.ForgetPassword)
-	 }
-	userRoute := r.Group("api/user")
-	{
-		userRoute.PATCH("/",userController.Login)
-	}
-	investorRoute := r.Group("api/investor")
-	{
-		investorRoute.PATCH("/",userController.Login)
-	}
-	borrowerRoute := r.Group("api/borrower")
-	{
-		borrowerRoute.PATCH("/",borrowerController.Login)
-	}
+	// authRoute := r.Group("api/auth")
+	// {
+	// 	authRoute.POST("/login",authController.Login)
+	// 	authRoute.POST("/register",authController.Register)
+	// 	authRoute.POST("/reset_password",authController.ForgetPassword)
+	// }
+	//userRoute := r.Group("api/user")
+	//{
+	//	userRoute.PATCH("/",userController.Login)
+	//}
+	//investorRoute := r.Group("api/investor")
+	//{
+	//	investorRoute.PATCH("/",userController.Login)
+	//}
+	//borrowerRoute := r.Group("api/borrower")
+	//{
+	//	borrowerRoute.PATCH("/",borrowerController.Login)
+	//}
 
 	 r.Run(":2020")
 }
